7f1ce565472f6a2ab0815d91dd2d2b896c39844d
flutter-lord <richardolojede2@gmail.com>
2024-10-09 10:51:50 +0100

    commit

--- bin/test_date_class.dart
+++ bin/test_date_class.dart
@@ -1,7 +1,7 @@
 import 'date_class.dart';
 
 void main() {
-  Date date = Date(2345637362);
+  Date date = Date(3333333333333);
 
   print('From 1970, $date milliSeconds is ${date.milliSecondsEporch()}');
 }
