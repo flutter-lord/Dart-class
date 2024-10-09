cc0cc679f8cca56766c2d3def3c2dec3415e66c2
flutter-lord <richardolojede2@gmail.com>
2024-10-09 11:24:28 +0100

    commit

--- bin/random_class.dart
+++ bin/random_class.dart
@@ -1,3 +1,24 @@
-class Random {
+class MyRandom {
+
+  int ?_randomNumber;
+
+  MyRandom(int randomNumber) {
+    _randomNumber = randomNumber;
+  }
+
+  set randomNumber (int randomNumber) {
+   
+   _randomNumber = randomNumber;
+    
+  }
+
+  int get randomNumber => _randomNumber!;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$_randomNumber';
+  }
+
   
 }
---
+++ bin/test_random_class.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+import 'random_class.dart';
+
+void main() {
+  
+  for (int i = 1; i <= 1000; i++) {
+    int i = Random().nextInt(101);
+
+    MyRandom myrandom = MyRandom(i);
+
+     print(myrandom);
+  }
+}
