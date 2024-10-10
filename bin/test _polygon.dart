3a7bdebd751eb5ff5eb10982b4c2fcfbce2b8285
flutter-lord <richardolojede2@gmail.com>
2024-10-10 15:56:13 +0100

    commit

--- bin/test _polygon.dart
+++ bin/test _polygon.dart
@@ -4,4 +4,8 @@ void main() {
   RegularPolygon regularpolygon = RegularPolygon(6, 4, 5.5, 6.8);
 
   print(regularpolygon);
+
+  print('The perimeter of the regular polygon is ${regularpolygon.getPerimeter()}');
+
+  print('The Area of the regular polygon is ${regularpolygon.getArea()}');
 }
