32efa98e8f47b7479d01b0f2ea88de7973cae7b4
flutter-lord <richardolojede2@gmail.com>
2024-10-10 23:16:24 +0100

    commit

--- bin/test_Quadrati.dart
+++ bin/test_Quadrati.dart
@@ -3,6 +3,6 @@ import 'quadratic-class.dart';
 void main() {
   QuadraticEquation quadraticEquation = QuadraticEquation(1, 7, 10);
 
-  print(quadraticEquation);
-  print(quadraticEquation.getDiscriminant());
+  //print(quadraticEquation);
+  print('The roots of the Equation ${quadraticEquation} is ${quadraticEquation.getDiscriminant()}');
 }
