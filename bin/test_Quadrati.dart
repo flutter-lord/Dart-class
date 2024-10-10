6eb163281291a80684a92c7f32e0572124701758
flutter-lord <richardolojede2@gmail.com>
2024-10-10 22:43:40 +0100

    commit

--- bin/test_Quadrati.dart
+++ bin/test_Quadrati.dart
@@ -1,8 +1,8 @@
 import 'quadratic-class.dart';
 
 void main() {
-  QuadraticEquation quadraticEquation = QuadraticEquation(1, 1 , -2);
+  QuadraticEquation quadraticEquation = QuadraticEquation(1, -2 , 1);
 
   print(quadraticEquation);
-  //print(quadraticEquation.getDiscriminant());
+  print(quadraticEquation.getDiscriminant());
 }
