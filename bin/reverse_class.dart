cd445cf7c1cbe90128ae7e4ab25a81d9cdbab322
flutter-lord <richardolojede2@gmail.com>
2024-10-10 11:44:31 +0100

    commit

--- bin/reverse_class.dart
+++ bin/reverse_class.dart
@@ -13,7 +13,7 @@ class ReverseString {
     String c = '';
 
     for (int i = a; i >= 0; i--) {
-      String b = _words.substring(i, ++i);
+      String b = _words.substring(i, (i++));
 
      c += b;
     }
