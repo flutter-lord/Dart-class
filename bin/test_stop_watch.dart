464c5f353821c25145efe882fe94d3a3979340dd
flutter-lord <richardolojede2@gmail.com>
2024-10-10 12:44:42 +0100

    commit

--- bin/test_stop_watch.dart
+++ bin/test_stop_watch.dart
@@ -1,12 +1,24 @@
 //import 'stop_watch_class.dart';
 
+import 'dart:io';
+
 void main() {
 //MyStopWatch mystopWatch = MyStopWatch();
+ stdout.write('Input A as many times as  possible:');
+ String A = stdin.readLineSync()!;
 
-int startTime = 1;
-int endtime = 2;
+  final _before =  DateTime.now();
 
-print('$startTime');
-print('$endtime');
+  String b = '''
+rdxcvbhfdxbnhtrdsz bgfcvbn  qcsbhxgsv wcdacavqbjskn wwcdsfwjhb
+qmhwgsfc wnhsgxfcs qxdafghsn ewxdtshn wdsftxghn cdfghb
+jhgfcsxbnhagfc qnshgxfcz
+''';
+
+  final _now = DateTime.fromMillisecondsSinceEpoch;
+ print(A);
+ print('$_before');
+ print(b);
+ print('$_now');
 
 }
