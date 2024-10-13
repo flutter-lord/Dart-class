853feef45d913977c795e22f7094d8899c8b305b
flutter-lord <richardolojede2@gmail.com>
2024-10-13 20:35:27 +0100

    commit

--- bin/password_class.dart
+++ bin/password_class.dart
@@ -1,46 +1,38 @@
 class Password {
-  String? _characters;
+  String _characters = '';
 
   Password(String characters) {
     _characters = characters;
   }
 
-  String get characters => _characters!;
+  String get characters => _characters;
 
-  int passwordLength() {
-    int _charactersLength = _characters!.length;
-    return _charactersLength;
-  }
 
   String a() {
-    String _alphabet = 'ABCDEFGHIJKLMNOPQRTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    String _alphabet = 'ABCDEFGHIJKLMNOPQRTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#%&*><|?^';
     return _alphabet; 
   }
 
-  String b() {
-    String _integer ='0123456789';
-    return _integer;
-  }
-
-  String c() {
-    String _symbol = '!@#%&*><|?^';
-    return _symbol;
+  bool passwordValid() {
+    
   }
 
   String checkPasswordStatus() {
-    if (passwordLength() >= 8 && passwordLength() <= 16 && _characters!.contains(a()) == true && _characters!.contains(b()) == true && _characters!.contains(c()) == true) {
-      return 'The password you have entered is valid';
-    }
 
-    else {
-      return 'The password you have entered is not valid';
-    }
+    int j = _characters.length;
+    int b = a().length;
 
+    for (int i = 0; i <= j; i++) {
+      for (int a = 0; a <= b; a++) {
+
+      }
+    }
   }
+
 
   @override
   String toString() {
     // TODO: implement toString
-    return '*' * passwordLength();
+    return '*' * characters.length;
   }
 }
