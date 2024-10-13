5309a25b232c36105cec9a12fe79e8070d2269a9
flutter-lord <richardolojede2@gmail.com>
2024-10-13 14:32:43 +0100

    commit

--- bin/password_class.dart
+++ bin/password_class.dart
@@ -4,4 +4,43 @@ class Password {
   Password(String characters) {
     _characters = characters;
   }
+
+  String get characters => _characters!;
+
+  int passwordLength() {
+    int _charactersLength = _characters!.length;
+    return _charactersLength;
+  }
+
+  String a() {
+    String _alphabet = 'ABCDEFGHIJKLMNOPQRTUVWXYZ';
+    return _alphabet; 
+  }
+
+  String b() {
+    String _integer ='0123456789';
+    return _integer;
+  }
+
+  String c() {
+    String _symbol = '!@#%&*><|?^';
+    return _symbol;
+  }
+
+  String checkPasswordStatus() {
+    if (passwordLength() >= 8 && passwordLength() <= 16 && _characters!.contains(a()) == true && _characters!.contains(b()) == true && _characters!.contains(c())== true) {
+      return 'The password you have entered is valid';
+    }
+
+    else {
+      return 'The password you have entered is not valid';
+    }
+
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '*' * passwordLength();
+  }
 }
---
+++ bin/test_password.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'password_class.dart';
+
+void main() {
+  stdout.write('Enter your password: ');
+  String characters = stdin.readLineSync()!;
+
+  Password password = Password('Richard@22');
+}
+
+// TestPassword
+// test_password
+// testPassword
